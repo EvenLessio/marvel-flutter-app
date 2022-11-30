b47e5e64e535f2e35b2db3aa7576cbe191bfe29d
Vechernin Rostislav <even644007@gmail.com>
2022-11-30 21:27:54 +0600

    fix: full main

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
+import 'package:fkn_labs_2022/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Marvel Heroes',
       theme: ThemeData(
-      floatingActionButton: FloatingActionButton(
         scaffoldBackgroundColor: const Color.fromARGB(255, 41, 37, 37),
   ),
+  home: const HomePage(),
     );
   }
 }
